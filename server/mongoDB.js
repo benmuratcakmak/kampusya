3716d964cd21bd7514b05c20890fd8a5c8f81608
Murat Cakmak <benmuratcakmak@gmail.com.com>
2024-12-07 22:56:02 +0300

    7 aralik cmt

--- server/mongoDB.js
+++ server/mongoDB.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose"
 
 const conn = () => {
     mongoose
-        .connect(process.env.MONGODB_URI_2)
+        .connect(process.env.MONGODB_URI)
+        // .connect(process.env.MONGODB_URI_PRODUCTION)
         .then(() => {
             console.log('mongoDb baglantisi basarili...')
         })
