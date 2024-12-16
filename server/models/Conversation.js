5bea5fb621fb27497b96b39617e01b512c11909e
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2024-12-17 01:50:55 +0300

    Model Dosyalarına model şemalarının temel yapısı için açıklamalar eklendi

--- server/models/Conversation.js
+++ server/models/Conversation.js
@@ -24,3 +24,16 @@ const ConversationSchema = new mongoose.Schema(
 
 const Conversation = mongoose.model("Conversation", ConversationSchema);
 export default Conversation;
+
+// ConversationSchema: Konuşma verisinin yapısını belirler.
+// participants: Konuşmadaki kullanıcıları belirler.
+// isRead: Konuşmanın okunup okunmadığını belirler.
+// lastMessage: Son mesajın ID'sini tutar.
+// timestamps: Verinin oluşturulma ve güncellenme tarihlerini otomatik ekler.
+// Bu model ile, bir konuşma (chat) sistemindeki kullanıcıların mesajlaştığı ve son mesajın takip edildiği verileri yönetebilirsiniz.,
+// Model:
+// Conversation: Bu şema bir Conversation modeline dönüştürülür. MongoDB'de bu modelin verileri conversations isimli koleksiyonda saklanır.
+// Kullanım:
+// participants kısmı, kullanıcıların kimler olduğunu belirler.
+// isRead konuşmanın okunup okunmadığını takip eder.
+// lastMessage son mesajın kim olduğunu belirler.
--- server/models/Message.js
+++ server/models/Message.js
@@ -30,3 +30,10 @@ const MessageSchema = new mongoose.Schema(
 
 const Message = mongoose.model("Message", MessageSchema);
 export default Message;
+
+// conversationId: Mesajın ait olduğu konuşmanın ID'sini tutar. Bu, hangi konuşma içinde olduğunu belirler. Conversation modeline referans (ref) verilir.
+// sender: Mesajı gönderen kullanıcının ID'sini tutar. Bu da User modeline referans verir.
+// text: Gönderilen mesajın metni (içeriği) burada tutulur.
+// sharePostId: Eğer mesajda bir paylaşım (post) varsa, bu alanda o paylaşımın ID'si tutulur. Bu alan opsiyonel (isteğe bağlı) olduğu için bazı mesajlar sadece metin içerebilir, bazıları ise bir gönderiyle birlikte olabilir.
+// timestamps: Bu özellik, mesajın ne zaman gönderildiğini ve en son ne zaman güncellendiğini otomatik olarak kaydeder.
+// Bu model sayesinde bir sohbet uygulamasında mesajları takip edebilir, her mesajın hangi konuşmaya ait olduğunu görebilir ve gönderen kişiyi belirleyebilirsiniz.
